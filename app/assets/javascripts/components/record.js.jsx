0caec5c0b60c6a9e4b1659a193fbe1a843ce7abc
Paul Cox <paulcox24@gmail.com>
2016-03-18 01:13:31 -0600

    Delete records

--- app/assets/javascripts/components/record.js.jsx
+++ app/assets/javascripts/components/record.js.jsx
@@ -1,10 +1,23 @@
 var Record = React.createClass({
+  handleDelete: function(e){
+    var props = this.props
+    e.preventDefault();
+    $.ajax({
+      method: 'DELETE',
+      url: "/records/" + props.record.id,
+      dataType: 'JSON'
+    })
+    .done(function(){
+      props.handleDeleteRecord(props.record);
+    });
+  },
   render: function(){
     return (
     <tr>
       <td>{this.props.record.date}</td>
       <td>{this.props.record.title}</td>
       <td>{amountFormat(this.props.record.amount)}</td>
+      <td><a className= "btn btn-danger" onClick={this.handleDelete}>Delete</a></td>
     </tr>
     );
   }
